eb4b5b27cf3c6c0324d0e2291691cc1711a10b1b
Ferransr <65186873+Ferransr@users.noreply.github.com>
2023-07-16 22:57:55 +0200

    ex3-Botones_Extras
    El calculo del total se realiza correctamente, se han probado todas las opciones.

--- src/components/presupuestoWeb.tsx
+++ src/components/presupuestoWeb.tsx
@@ -1,38 +1,50 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const PresupuestoWeb = () => {
   const [total, setTotal] = useState<number>(0);
   const [numPaginas, setNumPaginas] = useState<number>(0);
   const [numIdiomas, setNumIdiomas] = useState<number>(0);
-  const [extras, setextras] = useState<boolean>(false);
+  const [extras, setExtras] = useState<boolean>(false);
+  const [seoChecked, setSeoChecked] = useState<boolean>(false);
+  const [adsChecked, setAdsChecked] = useState<boolean>(false);
 
-  const updateTotal = (amount: number, isChecked: boolean) => {
-    setTotal((prevTotal) =>
-      isChecked ? prevTotal + amount : prevTotal - amount
-    );
-  };
+  const PRECIO_PAGINAS = 30;
+  const PRECIO_WEB = 500;
+  const PRECIO_SEO = 300;
+  const PRECIO_ADS = 200;
 
   useEffect(() => {
-    const precioPaginas = numPaginas * numIdiomas * 30;
-    const SEO_PRICE = 300;
-    const ADS_PRICE = 200;
-    setTotal(precioPaginas + (extras ? SEO_PRICE : 0) + (extras ? ADS_PRICE : 0));
-  }, [numPaginas, numIdiomas, extras]);
+    const precioPaginas = extras ? numPaginas * numIdiomas * PRECIO_PAGINAS : 0;
+    const precioWeb = extras ? PRECIO_WEB : 0;
+    const precioSeo = seoChecked ? PRECIO_SEO : 0;
+    const precioAds = adsChecked ? PRECIO_ADS : 0;
 
-  const handleNumPaginasChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(e.target.value);
-    if (!isNaN(value) && value >= 0) {
-      setNumPaginas(value);
+    const totalCalculado = precioPaginas + precioWeb + precioSeo + precioAds;
+    const totalValidado = Math.max(totalCalculado, 0);
+
+    setTotal(totalValidado);
+  }, [numPaginas, numIdiomas, extras, seoChecked, adsChecked]);
+
+  const handleNumPaginasChange = (increment: number) => {
+    const newValue = numPaginas + increment;
+    if (newValue >= 0) {
+      setNumPaginas(newValue);
     }
-    console.log("paginas cambio");
   };
 
-  const handleNumIdiomasChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(e.target.value);
-    if (!isNaN(value) && value >= 0) {
-      setNumIdiomas(value);
+  const handleNumIdiomasChange = (increment: number) => {
+    const newValue = numIdiomas + increment;
+    if (newValue >= 0) {
+      setNumIdiomas(newValue);
     }
-    console.log("idiomas cambio");
+  };
+
+  const handleSeoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSeoChecked(e.target.checked);
+  };
+
+  const handleAdsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAdsChecked(e.target.checked);
   };
 
   return (
@@ -42,10 +54,7 @@ const PresupuestoWeb = () => {
         <input
           type="checkbox"
           id="checkbox1"
-          onChange={(e) => {
-            updateTotal(500, e.target.checked);
-            setextras(e.target.checked);
-          }}
+          onChange={() => setExtras(!extras)}
           className="mr-2"
         />
         <label htmlFor="checkbox1">Una página web (500€)</label>
@@ -53,23 +62,39 @@ const PresupuestoWeb = () => {
           <>
             <div>
               <label htmlFor="numPaginas">Número de páginas:</label>
-              <input
-                type="number"
-                id="numPaginas"
-                onChange={handleNumPaginasChange}
-                value={numPaginas}
-                className="mr-2 ml-2"
-              />
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => handleNumPaginasChange(1)}
+              >
+                +
+              </button>
+              <span className="mr-2 ml-2">{numPaginas}</span>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => handleNumPaginasChange(-1)}
+              >
+                -
+              </button>
             </div>
             <div>
               <label htmlFor="numIdiomas">Número de idiomas:</label>
-              <input
-                type="number"
-                id="numIdiomas"
-                onChange={handleNumIdiomasChange}
-                value={numIdiomas}
-                className="mr-2 ml-2"
-              />
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => handleNumIdiomasChange(1)}
+              >
+                +
+              </button>
+              <span className="mr-2 ml-2">{numIdiomas}</span>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() => handleNumIdiomasChange(-1)}
+              >
+                -
+              </button>
             </div>
           </>
         )}
@@ -78,16 +103,18 @@ const PresupuestoWeb = () => {
         <input
           type="checkbox"
           id="checkbox2"
-          onChange={(e) => updateTotal(300, e.target.checked)}
+          checked={seoChecked}
+          onChange={handleSeoChange}
           className="mr-2"
         />
-        <label htmlFor="checkbox2">Una consultoria SEO (300€)</label>
+        <label htmlFor="checkbox2">Una consultoría SEO (300€)</label>
       </div>
       <div>
         <input
           type="checkbox"
           id="checkbox3"
-          onChange={(e) => updateTotal(200, e.target.checked)}
+          checked={adsChecked}
+          onChange={handleAdsChange}
           className="mr-2"
         />
         <label htmlFor="checkbox3">Una campaña de Google Ads (200€)</label>
